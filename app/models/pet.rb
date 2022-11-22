e027eacd2f9a14eb9c7427bc2d05e33970650eee
Mayté Mejía <maytemejia@live.com.mx>
2022-11-21 20:47:01 -0600

    Add basic validations for pet model and test

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Pet < ApplicationRecord
+  validates :name, :genre, :specie, :color,
+            :brees, :particular_sings, presence: true
+  validates :genre, inclusion: { in: %w[female male],
+                                 message: "%<value>s isn't valid in list" }
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,11 +1,33 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Pet, type: :model do
-  let(:pet) { FactoryBot.create(:pet) }
-  
-  describe 'Pet with valid values' do 
-    it 'create a new register' do
-      expect(Pet.all.count).to eq(1)
+  before { create(:pet) }
+
+  describe 'Create a pet record with valid values' do
+    it 'return a register' do
+      expect(Pet.count).to eq(1)
+    end
+
+    it do
+      pet = Pet.last
+      pet.genre = 'undefined'
+      expect(pet).to validate_inclusion_of(:genre).in_array(%w[female male])
+    end
+  end
+
+  describe 'Invalid pet data' do
+    let(:pet) { build(:pet) }
+    it 'return error if pet has not name' do
+      pet.name = nil
+      expect(pet.save).to eq(false)
+    end
+
+    it 'return error if pet has not color' do
+      pet.color = nil
+      pet.save
+      expect(pet.errors.full_messages).to eq(["Color can't be blank"])
     end
   end
 end
